6e4c75f9899765463797b07b4385cd95cbce4e09
Punyugi2012 <58160216@go.buu.ac.th>
2018-08-16 13:44:30 +0700

    Initial Commit

---
+++ ItemRecognizer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ItemRecognizer
+//
+//  Created by punyawee  on 16/8/61.
+//  Copyright © พ.ศ. 2561 Punyugi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
